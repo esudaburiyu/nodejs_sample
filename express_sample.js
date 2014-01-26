57f06ef71981e5d0954e76347fbfa8a8e0b27e75
barutanGNE <esudaburiyu@gmail.com>
2014-01-26 19:43:57 +0900

    index File added

--- express_sample.js
+++ express_sample.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/public/index.html');
+});
+
 app.listen(process.env.PORT, process.env.IP);
+// app.listen(3000);
 console.log("server starting...");
